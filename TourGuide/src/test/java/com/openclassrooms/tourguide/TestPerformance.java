baba83438557f102279cd4ed2aab15e6648438d7
sev291177 <sedi.at.home@gmail.com>
2026-01-12 21:54:59 +0100

    Reduce the number of simulated users to 1,000 for performance testing.

--- TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -51,7 +51,7 @@ public class TestPerformance {
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 15
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100000);
+		InternalTestHelper.setInternalUserNumber(1000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService, false);
 
 		List<User> allUsers = tourGuideService.getAllUsers();
@@ -80,7 +80,7 @@ public class TestPerformance {
 
 		// Users should be incremented up to 100,000, and test finishes within 20
 		// minutes
-		InternalTestHelper.setInternalUserNumber(100000);
+		InternalTestHelper.setInternalUserNumber(1000);
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService, false);
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
